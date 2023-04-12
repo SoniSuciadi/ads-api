1b01424debfff4388841f2999e4a07eef246b5d2
SoniSuciadi <sonisuciadi@gmail.com>
2023-04-13 00:47:50 +0700

    hot fix script start

--- configs/facebook.js
+++ configs/facebook.js
@@ -1,5 +1,9 @@
 import { FacebookAdsApi, AdAccount } from "facebook-nodejs-business-sdk";
 export const facebookConfig = (accessToken, adAccount) => {
+  console.log(
+    "👻 ~ file: facebook.js:3 ~ facebookConfig ~ accessToken:",
+    accessToken
+  );
   FacebookAdsApi.init(accessToken);
   return new AdAccount(adAccount);
 };
