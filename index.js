19d7a756d67c0c3f3973fd07204150e828ab0098
SoniSuciadi <sonisuciadi@gmail.com>
2023-04-13 10:23:35 +0700

    ok

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 80;
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(req.headers, "--------------------");
+  next();
+});
 app.use(morganHttpLogger);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
